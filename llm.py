9ab3e3071cbfb27e249e43a3d31e8c66fa3ae4d5
Murugan Ganesan <rushmurugan@gmail.com>
2025-12-15 11:06:08 +0530

    Update llm.py

--- llm.py
+++ llm.py
@@ -1,10 +1,10 @@
-import os
+import streamlit as st
 import google.generativeai as genai
-from dotenv import load_dotenv
 
-load_dotenv()
-
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+# Configure Gemini using Streamlit Secrets
+genai.configure(
+    api_key=st.secrets["GEMINI_API_KEY"]
+)
 
 MODEL_NAME = "gemini-1.5-flash"
 
